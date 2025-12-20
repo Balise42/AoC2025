338899c43c7a00a1d3e64b111f1c978dee3f9450
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2025-12-20 18:27:44 +0100

    day 10, still a large *meh* (too slow)

--- day10.py
+++ day10.py
@@ -1,5 +1,97 @@
 import re
-import numpy as np
+import copy
+import random
+
+class Light:
+    target = 0
+    toggles = []
+    
+    def __init__(self, target, toggles):
+        self.target = target
+        self.toggles = toggles
+
+    def click(self,toggle, times):
+        if toggle in self.toggles:
+            self.target -= times
+            self.toggles.remove(toggle)
+    
+    def isValid(self):
+        if len(self.toggles) == 0:
+            return self.target == 0
+        return self.target >= 0
+
+    def __str__(self):
+        return "{} {}".format(self.target, self.toggles)
+    
+
+class State:
+    lights = []
+    clicks = {}
+
+    def __init__(self, lights):
+        self.lights = lights
+
+    def __str__(self):
+        res = ""
+        for i in self.lights:
+            res += "{}   ".format(i)
+        res += " {}".format(self.clicks)
+        return res
+    
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(other):
+        return str(other) == str(self)
+
+    def click(self):
+        self.lights.sort(key=lambda l: len(l.toggles)*1000 + l.target)
+        light = self.lights[0]
+        if light.target == 0:
+            self.lights.pop(0)
+            return [self]
+        if len(light.toggles) == 1:
+            self.lights.pop(0)
+            nl = []
+            for l in self.lights:
+                nl.append(Light(l.target, l.toggles.copy()))
+            ns = State(nl)
+            ns.clicks = self.clicks.copy()
+            ns = ns.clickToggle(light.toggles[0], light.target)
+            if (ns.isValid()):
+                return [ns]
+            else:
+                return []
+
+        res = []
+        tog = light.toggles[0]
+        for i in range(0, light.target+1):
+            nl = []
+            for l in self.lights:
+                nl.append(Light(l.target, l.toggles.copy()))
+            ns = State(nl)
+            ns.clicks = self.clicks.copy()
+            att = ns.clickToggle(tog, i)
+            if att.isValid():
+                res.append(att)
+            else:
+                break
+        return res
+
+    def clickToggle(self, toggle, times):
+        for light in self.lights:
+            light.click(toggle, times)
+        self.clicks[toggle] = times
+        return self
+ 
+
+    def isValid(self):
+        for light in self.lights:
+            if not light.isValid():
+                return False
+        return True
+
+    
 
 def main():
     lines = open('data/day10.txt', 'r').readlines()
@@ -31,14 +123,33 @@ def main():
                     break
         
         voltageList = [int(a) for a in strVoltages.split(',')]
-        matrix = [[0 for i in range(len(toggleList))] for j in range(len(voltageList))]
-        for i in range(len(toggleList)):
-            t = toggleList[i]
-            for j in range(len(t)):
-                matrix[t[j]][i] = 1
-        matrix = np.matrix(matrix)
-        print(matrix)
-        print(voltageList)
+
+        lights = []
+
+        for i in range(len(voltageList)):
+            v = voltageList[i]
+            light = Light(v, [])
+            for j in range(len(toggleList)):
+                toggle = toggleList[j]
+                if i in toggle:
+                    light.toggles.append(j)
+
+            lights.append(light)
+        
+        state = State(lights)
+        states = [state]
+        num = 1000000
+        while len(states) > 0:
+            s = states.pop(0)
+            if len(s.lights) == 0:
+                cand = sum(s.clicks.values())
+                if (num > cand):
+                    num = cand
+                continue
+            states.extend(s.click())
+        res2 += num
+        print(num)
+
     print(res)
     print(res2)
 
